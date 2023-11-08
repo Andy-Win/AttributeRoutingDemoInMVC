7f6dc17076eb09467ac9eaaf3330988e84213add
Andy Wang <v-wajian@microsoft.com>
2023-11-08 14:22:20 +0800

    12

---
+++ AttributeRoutingDemoInMVC/Areas/Teacher/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AttributeRoutingDemoInMVC.Areas.Teacher.Controllers
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        // GET: Teacher/Home
+        public ActionResult GetTeachers()
+        {
+            List<Teacher> teachers = new List<Teacher>()
+            {
+                new Teacher() { Id = 1, Name = "James" },
+                new Teacher() { Id = 2, Name = "Patrik" },
+                new Teacher() { Id = 3, Name = "Smith" }
+            };
+            return View(teachers);
+        }
+    }
+}
